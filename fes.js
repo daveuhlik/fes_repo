5ce96a1cffce002977d1e635355246ebef6e34b9
Dave Uhlik <49681728+daveuhlik@users.noreply.github.com>
2020-11-20 18:59:49 +0100

    Add files via upload
    First commit

---
+++ fes.js
@@ -0,0 +1,11 @@
+// let zsolt = document.getElementById('zsolt')
+// let color = document.getElementsByClassName('color')
+
+function hideElement() {
+	let zsolt = document.getElementById('zsolt')
+	if (zsolt.style.display === "block") {
+		zsolt.style.display = "none"
+	} else {
+		zsolt.style.display = "block"
+	}
+}
