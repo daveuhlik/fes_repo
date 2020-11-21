b3b628f18f6b932c1d42824fdc99936996772123
Dave Uhlik <49681728+daveuhlik@users.noreply.github.com>
2020-11-21 16:07:49 +0100

    Add files via upload

--- fes.js
+++ fes.js
@@ -1,14 +1,27 @@
-
-
-var myImage = document.querySelector('img');
+var zsoltImage = document.getElementById('zsolt');
+var anitaImage = document.getElementById('anita');
 
 $(function(){ 
-   myImage.onclick = function() {
-        var mySrc = myImage.getAttribute('src');
+   zsoltImage.onclick = function() {
+        var mySrc = zsoltImage.getAttribute('src');
         if(mySrc === 'img/zsolt.gif') {
-            myImage.setAttribute ('src','img/zsolt.png');
+            zsoltImage.setAttribute ('src','img/zsolt.png');
          } else {
-            myImage.setAttribute ('src','img/zsolt.gif');
+            zsoltImage.setAttribute ('src','img/zsolt.gif');
          }
    };
 });
+
+
+
+$(function(){ 
+   anitaImage.onclick = function() {
+        var mySrc = anitaImage.getAttribute('src');
+        if(mySrc === 'img/anita.gif') {
+            anitaImage.setAttribute ('src','img/anita.png');
+         } else {
+            anitaImage.setAttribute ('src','img/anita.gif');
+         }
+   };
+});
+
