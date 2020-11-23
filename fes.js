75f71176be9cbf196a26871628d69b06b6f1c2e2
Dave Uhlik <49681728+daveuhlik@users.noreply.github.com>
2020-11-23 10:40:14 +0100

    Add files via upload
    Modified to start with png's instead of gif's.

--- fes.js
+++ fes.js
@@ -6,10 +6,10 @@ var csillaImage = document.getElementById('csilla');
 $(function(){ 
    zsoltImage.onclick = function() {
         var mySrc = zsoltImage.getAttribute('src');
-        if(mySrc === 'img/zsolt.gif') {
-            zsoltImage.setAttribute ('src','img/zsolt.png');
-         } else {
+        if(mySrc === 'img/zsolt.png') {
             zsoltImage.setAttribute ('src','img/zsolt.gif');
+         } else {
+            zsoltImage.setAttribute ('src','img/zsolt.png');
          }
    };
 });
@@ -19,10 +19,10 @@ $(function(){
 $(function(){ 
    anitaImage.onclick = function() {
         var mySrc = anitaImage.getAttribute('src');
-        if(mySrc === 'img/anita.gif') {
-            anitaImage.setAttribute ('src','img/anita.png');
-         } else {
+        if(mySrc === 'img/anita.png') {
             anitaImage.setAttribute ('src','img/anita.gif');
+         } else {
+            anitaImage.setAttribute ('src','img/anita.png');
          }
    };
 });
@@ -30,10 +30,10 @@ $(function(){
 $(function(){ 
    mariannaImage.onclick = function() {
         var mySrc = mariannaImage.getAttribute('src');
-        if(mySrc === 'img/marianna.gif') {
-            mariannaImage.setAttribute ('src','img/marianna.png');
-         } else {
+        if(mySrc === 'img/marianna.png') {
             mariannaImage.setAttribute ('src','img/marianna.gif');
+         } else {
+            mariannaImage.setAttribute ('src','img/marianna.png');
          }
    };
 });
